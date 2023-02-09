cb52c88bd71afa28e09282acd21e5fcccf3f2e33
Nicolai Arnesen <nicolaar@stud.ntnu.no>
2023-02-09 11:31:10 +0100

    Backend login
    co-authored-by: Knut Lilleås

---
+++ backend/src/login.ts
@@ -0,0 +1,29 @@
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const auth = getAuth();
+signInAnonymously(auth)
+  .then(() => {
+    // Signed in..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
