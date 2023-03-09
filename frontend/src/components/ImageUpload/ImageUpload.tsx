5ea21541221f4652e07e5cc1466326444e736f6a
Knut A <kalillea@stud.ntnu.no>
2023-03-09 11:05:29 +0100

    Fiksferdig

--- frontend/src/components/ImageUpload/ImageUpload.tsx
+++ frontend/src/components/ImageUpload/ImageUpload.tsx
@@ -1,136 +1,32 @@
-import { useState, useEffect, useRef } from "react";
-import UploadService from "../../services/FileUploadService";
-import IFile from "../../types/File";
+import { ChangeEvent } from "react";
 
-const ImageUpload: React.FC = () => {
-  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
-  const [imagePreviews, setImagePreviews] = useState<Array<string>>([]);
-  const [message, setMessage] = useState<Array<string>>([]);
-  const [imageInfos, setImageInfos] = useState<Array<IFile>>([]);
+interface Props {
+  setImageIds: React.Dispatch<React.SetStateAction<string[]>>;
+  setFiles: React.Dispatch<React.SetStateAction<FileList | null>>;
+}
 
-  const selectImages = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let images: Array<string> = [];
-    let files = event.target.files;
+const ImageUpload = ({ setImageIds, setFiles }: Props) => {
+  const handleFilesChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFiles = event.currentTarget.files;
+    setFiles(selectedFiles);
+    setImageIds([]);
 
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        images.push(URL.createObjectURL(files[i]));
-      }
-
-      setSelectedFiles(files);
-      setImagePreviews(images);
-      setMessage([]);
-    }
-  };
-
-  useEffect(() => {
-    UploadService.getFiles().then((response) => {
-      setImageInfos(response.data);
-    });
-  }, []);
-
-  const uploadImages = () => {
-    if (selectedFiles != null) {
-      const files = Array.from(selectedFiles);
-
-      let _progressInfos = files.map((file) => ({
-        percentage: 0,
-        fileName: file.name,
-      }));
-
-
-      const uploadPromises = files.map((file, i) => upload(i, file));
-
-      Promise.all(uploadPromises)
-        .then(() => UploadService.getFiles())
-        .then((images) => {
-          setImageInfos(images.data);
-        });
-
-      setMessage([]);
-    }
-  };
-
-  const upload = (idx: number, file: File) => {
-    return UploadService.upload(file)
-      .then(() => {
-        setMessage((prevMessage) => [
-          ...prevMessage,
-          file.name + ": Successful!",
-        ]);
-      })
-      .catch((err: any) => {
-        let msg = file.name + ": Failed!";
-        if (err.response && err.response.data && err.response.data.message) {
-          msg += " " + err.response.data.message;
-        }
-
-        setMessage((prevMessage) => [...prevMessage, msg]);
+    for (let i = 0; i < (selectedFiles?.length || 0); i++) {
+      const id = crypto.randomUUID();
+      setImageIds((prev) => {
+        return [...prev, id];
       });
+    }
   };
 
   return (
-    <div>
-
-      <div className="row my-3">
-        <div className="col-8">
-          <label className="btn btn-default p-0">
-            <input
-              type="file"
-              multiple
-              accept="image/*"
-              onChange={selectImages}
-            />
-          </label>
-        </div>
-
-        <div className="col-4">
-          <button
-            className="btn btn-success btn-sm"
-            disabled={!selectedFiles}
-            onClick={uploadImages}
-          >
-            Upload
-          </button>
-        </div>
-      </div>
-
-      {imagePreviews && (
-        <div>
-          {imagePreviews.map((img, i) => {
-            return (
-              <img className="preview" src={img} alt={"image-" + i} width="250px" key={i} />
-            );
-          })}
-        </div>
-      )}
-
-      {message.length > 0 && (
-        <div className="alert alert-secondary mt-2" role="alert">
-          <ul>
-            {message.map((item, i) => {
-              return <li key={i}>{item}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-
-      {imageInfos.length > 0 && (
-        <div className="card mt-3">
-          <div className="card-header">List of Images</div>
-          <ul className="list-group list-group-flush">
-            {imageInfos.map((img, index) => (
-              <li className="list-group-item" key={index}>
-                <p>
-                  <a href={img.url}>{img.name}</a>
-                </p>
-                <img src={img.url} alt={img.name} height="80px" />
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <input
+      type="file"
+      name="image"
+      onChange={handleFilesChanged}
+      accept="image/*"
+      multiple={true}
+    />
   );
 };
 
--- frontend/src/pages/CreateTripForm/CreateTripForm.tsx
+++ frontend/src/pages/CreateTripForm/CreateTripForm.tsx
@@ -1,17 +1,20 @@
-import React, { FormEvent } from "react";
+import { FormEvent, useContext, useState } from "react";
 import ImageUpload from "../../components/ImageUpload/ImageUpload";
 import "./CreateTripForm.css";
+import { UserContext } from "../../authentication/UserProvider";
+import { TripData } from "../../types/Trip";
+import { uploadFile } from "../../storage/util/methods";
 
 interface CustomElements extends HTMLFormControlsCollection {
-  startDestinasjon: HTMLInputElement;
-  Reisemål: HTMLInputElement;
-  Land: HTMLInputElement;
-  Pris: HTMLInputElement;
-  Reisetid: HTMLInputElement;
-  Vurdering: HTMLInputElement;
-  Klima: HTMLInputElement;
-  Lengde: HTMLInputElement;
-  Beskrivelse: HTMLInputElement;
+  startCity: HTMLInputElement;
+  destinationCity: HTMLInputElement;
+  countries: HTMLInputElement;
+  price: HTMLInputElement;
+  tripDurationDays: HTMLInputElement;
+  degreesCelcius: HTMLInputElement;
+  tripLengthKm: HTMLInputElement;
+  description: HTMLInputElement;
+  attractions: HTMLInputElement;
 }
 
 interface CustomForm extends HTMLFormElement {
@@ -19,23 +22,55 @@ interface CustomForm extends HTMLFormElement {
 }
 
 export const CreateTripForm = () => {
+  const [imageIds, setImageIds] = useState<string[]>([]);
+  const [files, setFiles] = useState<FileList | null>(null);
+  const { currentUser } = useContext(UserContext);
+
+  const uploadFiles = async (tripId: string) => {
+    for (let i = 0; i < (files?.length || 0); i++) {
+      const file = files?.item(0);
+      if (
+        file?.type !== "image/png" &&
+        file?.type !== "image/jpeg" &&
+        file?.type !== "image/jpg" &&
+        file?.type !== "image/jfif"
+      ) {
+        alert("Bildet må være av typen PNG, JPG eller JPEG");
+        return;
+      }
+
+      const path = `trips/${tripId}/${imageIds[i]}`;
+
+      await uploadFile(file, path);
+    }
+  };
+
   const onSubmit = (event: FormEvent<CustomForm>) => {
     event.preventDefault();
     const target = event.currentTarget.elements;
 
-    const data = {
-      startDestinasjon: target.startDestinasjon.value,
-      Reisemål: target.Reisemål.value,
-      Land: target.Land.value,
-      Pris: target.Pris.value,
-      Reisetid: target.Reisetid.value,
-      Vurdering: target.Vurdering.value,
-      Klima: target.Klima.value,
-      Lengde: target.Lengde.value,
-      Beskrivelse: target.Beskrivelse.value,
+    const data: TripData = {
+      startCity: target.startCity.value,
+      destinationCity: target.destinationCity.value,
+      countries: target.countries.value.split(new RegExp(", +")),
+      price: parseInt(target.price.value),
+      tripDurationDays: parseInt(target.tripDurationDays.value),
+      degreesCelcius: parseInt(target.degreesCelcius.value),
+      tripLengthKm: parseInt(target.tripLengthKm.value),
+      description: target.description.value,
+      attractions: target.attractions.value.split(new RegExp(", +")),
+
+      imageURLs: imageIds,
+      postDate: Date.now().toString(),
+      posterUID: currentUser?.userUid!,
     };
 
     console.log(data);
+    // Her vil den sende et API kall for å laste opp reisen
+    // Den må så få sendt tilbake den gitte IDen for reisen (gitt av Firebase)
+    // ===== Eksempel ======
+    // const tripId = createTrip(data);
+    // uploadFiles(tripId);
   };
 
   return (
@@ -43,82 +78,88 @@ export const CreateTripForm = () => {
       <div className="left-container-d">
         <div className="title">Generelt</div>
         <div className="field">
-          <label htmlFor="startDestinasjon">Start</label>
+          <label htmlFor="startCity">Start By</label>
           <input
             className="input-box"
-            id="startDestinasjon"
-            placeholder="By, turistområde, osv..."
+            id="startCity"
+            placeholder="Roma, Stavanger, Cape Town..."
             required
           />
         </div>
         <div className="field">
-          <label htmlFor="Reisemål">Reisemål</label>
+          <label htmlFor="destinationCity">Slutt By</label>
           <input
             className="input-box"
-            type="Reisemål"
-            id="Reisemål"
-            placeholder="By, turistområde, osv..."
+            id="destinationCity"
+            placeholder="Firenze, Oslo, Mubishu..."
             required
           />
         </div>
         <div className="field">
-          <label htmlFor="Land">Land</label>
+          <label htmlFor="countries">Land</label>
           <input
             className="input-box"
-            type="Land"
-            id="Land"
+            id="countries"
             placeholder="Norge, USA, Island, osv..."
             required
           />
         </div>
         <div className="field">
-          <label htmlFor="Pris">Pris</label>
+          <label htmlFor="price">Pris (kroner)</label>
           <input
             className="input-box"
-            type="Pris"
-            id="Pris"
-            placeholder="Eks. 10000kr, 30000kr,..."
+            type="number"
+            min="0"
+            max="999999"
+            id="price"
+            placeholder="Eks. 4500, 15000,..."
             required
           />
         </div>
         <div className="field">
-          <label htmlFor="Reisetid">Reisetid</label>
+          <label htmlFor="tripDurationDays">Varighet (dager)</label>
           <input
             className="input-box"
-            type="Reisetid"
-            id="Reisetid"
-            placeholder="Eks. 2-3 dager..."
-            required
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="Vurdering">Vurdering</label>
-          <input
-            className="input-box"
-            type="Vurdering"
-            id="Vurdering"
-            placeholder="Rangering fra 1-5..."
+            type="number"
+            min="0"
+            max="365"
+            id="tripDurationDays"
+            placeholder="Eks. 2, 3, 10..."
             required
           />
         </div>
         <div className="title">Detaljer</div>
         <div className="field">
-          <label htmlFor="Klima">Klima</label>
+          <label htmlFor="degreesCelcius">Temperatur (grader)</label>
           <input
             className="input-box"
-            type="Klima"
-            id="Klima"
+            type="number"
+            min="-100"
+            max="70"
+            step="0.1"
+            id="degreesCelcius"
             placeholder="Forventet temperatur..."
             required
           />
         </div>
         <div className="field">
-          <label htmlFor="Lengde">Lengde</label>
+          <label htmlFor="tripLengthKm">Lengde (i kilometer)</label>
           <input
             className="input-box"
-            type="Lengde"
-            id="Lengde"
+            type="number"
+            min="0"
+            max="40000"
+            id="tripLengthKm"
             placeholder="Distanse i kilometer..."
+            required
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="attractions">Attraksjoner (i kilometer)</label>
+          <input
+            className="input-box"
+            id="attractions"
+            placeholder="Eiffeltårnet, Sfinx, Colosseum..."
             required
           />
         </div>
@@ -139,14 +180,14 @@ export const CreateTripForm = () => {
         <div className="large-field">
           <textarea
             className="input-description"
-            id="Beskrivelse"
+            id="description"
             placeholder="Skriv litt om reisen din her..."
             required
           ></textarea>
         </div>
         <div className="title">Bilder</div>
         <div className="image-upload">
-          <ImageUpload />
+          <ImageUpload {...{ setImageIds, setFiles }} />
         </div>
       </div>
       <button className="submit-button" type="submit">
