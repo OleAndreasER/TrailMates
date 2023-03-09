96ff457f6b2f980b1c441e8205e7abf49ce5bc06
Knut A <kalillea@stud.ntnu.no>
2023-03-09 12:06:26 +0100

    Deleted redudant Terms&Conditions

--- frontend/src/pages/TripForm/TripForm.tsx
+++ frontend/src/pages/TripForm/TripForm.tsx
@@ -170,9 +170,6 @@ export const TripForm = () => {
             id="conditionsAccepted"
             required
           />
-          <label htmlFor="conditionsAccepted">
-            I agree to the terms and conditions
-          </label>
         </div>
       </div>
       <div className="right-container-d">
